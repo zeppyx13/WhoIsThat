76755f32cdb1fc6702e188196581ad62a80522c8
Gung Nanda <gungnands@gmail.com>
2024-11-14 05:32:44 +0800

    hanya confidence_level >  0.5 yang disimpan

--- main_video.py
+++ main_video.py
@@ -17,20 +17,22 @@ last_detection_time = {}
 
 def save_detection_to_db(name, location=None, confidence_level=None):
     try:
-        # Convert confidence_level to float
-        if confidence_level is not None:
+        # Hanya simpan jika confidence_level > 0.5
+        if confidence_level is not None and confidence_level > 0.5:
+            # Konversi confidence_level ke float (Python native)
             confidence_level = float(confidence_level)
-        # Db process to save the detection
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
-        query = '''
-            INSERT INTO detected_faces (name, location, confidence_level)
-            VALUES (%s, %s, %s)
-        '''
-        cursor.execute(query, (name, location, confidence_level))
-        conn.commit()
-        cursor.close()
-        conn.close()
+
+            # Proses penyimpanan ke database
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor()
+            query = '''
+                INSERT INTO detected_faces (name, location, confidence_level)
+                VALUES (%s, %s, %s)
+            '''
+            cursor.execute(query, (name, location, confidence_level))
+            conn.commit()
+            cursor.close()
+            conn.close()
     except mysql.connector.Error as err:
         print(f"Error: {err}")
 
@@ -64,11 +66,12 @@ def process_frame():
 
             # Pastikan wajah tidak terlalu dekat dengan kamera
             if (x2 - x1) > 100 and (y2 - y1) > 100:  # Cek ukuran minimal wajah
-                cv2.putText(frame, f"{name} ({confidence:.2f})", (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+                cv2.putText(frame, f"{name} ({confidence:.2f})", (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200),
+                            2)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
 
                 # Periksa jeda waktu sebelum input data yang sama
-                if name not in last_detection_time or (current_time - last_detection_time[name] > 5):
+                if name not in last_detection_time or (current_time - last_detection_time[name] > 4):
                     save_detection_to_db(name, 'Camera 1', confidence)
                     last_detection_time[name] = current_time  # Perbarui waktu deteksi terakhir
 
